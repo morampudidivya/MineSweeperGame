b232423143463a731ebf5be5b287de7d5432056f
DivyaMorampudi <dmorampudi@caci.co.uk>
2024-10-28 21:29:20 +0000

    second commit

---
+++ game.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("game.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CACI")]
+[assembly: AssemblyProduct("game.Test")]
+[assembly: AssemblyCopyright("Copyright © CACI 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9bb1e515-5a79-4b33-b36c-58d0babe0e58")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
